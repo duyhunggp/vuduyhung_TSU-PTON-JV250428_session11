53ad016cb6d942cdb37425b3f1bd084d09253e23
duyhunggp <vuduyhunggp.1998@gmail.com>
2025-06-09 17:56:26 +0900

    uoload file

---
+++ baitap3/index.js
@@ -0,0 +1,35 @@
+let number = +prompt("nhap mot so tu 0 den 9: ");
+switch (number) {
+  case 0:
+    alert("so khong");
+    break;
+  case 1:
+    alert("so mot");
+    break;
+  case 2:
+    alert("so hai");
+    break;
+  case 3:
+    alert("so ba");
+    break;
+  case 4:
+    alert("so bon");
+    break;
+  case 5:
+    alert("so nam");
+    break;
+  case 6:
+    alert("so sau");
+    break;
+  case 7:
+    alert("so bay");
+    break;
+  case 8:
+    alert("so tam");
+    break;
+  case 9:
+    alert("so chin");
+    break;
+  default:
+    alert("so ban vua nhap khong dung");
+}
